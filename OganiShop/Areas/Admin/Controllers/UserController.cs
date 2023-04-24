66f255a48179f9a8ce6063075e58d3ab7f46c7cc
ADMIN <tt2324564@gmail.com>
2023-04-25 04:57:12 +0700

    Phân quyền

--- OganiShop/Areas/Admin/Controllers/UserController.cs
+++ OganiShop/Areas/Admin/Controllers/UserController.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using OganiShop.Entities;
 using OganiShop.Helpers;
+using System.Security.Claims;
 
 namespace OganiShop.Areas.Admin.Controllers
 {
@@ -13,16 +15,49 @@ namespace OganiShop.Areas.Admin.Controllers
         private readonly OganiShopContext _dbContext;
         private readonly IMapper _mapper;
         private readonly IFileStorageService _fileStorageService;
-        private readonly string containerName = "UserImage";
+        private readonly string containerName = "account";
         public UserController(OganiShopContext dbContext, IMapper mapper, IFileStorageService fileStorageService)
         {
             _dbContext = dbContext;
             _mapper = mapper;
             _fileStorageService = fileStorageService;
         }
+
+        public string GetAccount()
+        {
+            var claims = HttpContext.User.Identity as ClaimsIdentity;
+            if (claims != null)
+            {
+                var account = claims.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                if (account != null)
+                {
+                    return account;
+                }
+            }
+            return string.Empty;
+        }
         public IActionResult Index()
         {
-            return View();
+            var temp = _dbContext.Users.Where(x => x.IsDeleted == false).Include(x => x.AccountNavigation);
+            return View(temp);
         }
+
+        public IActionResult SetAdmin(int Id)
+        {
+            var entity = _dbContext.Users.Include(x => x.AccountNavigation).SingleOrDefault(x => x.Id == Id);
+            if (entity == null)
+            {
+                TempData["Message"] = "Can't Set Admin";
+                return RedirectToAction("Index");
+            }
+
+            entity.AccountNavigation.Role = "admin";
+            entity.AccountNavigation.UpdatedBy = GetAccount();
+            entity.AccountNavigation.UpdatedDate = DateTime.Now;
+            _dbContext.SaveChanges();
+            TempData["Message"] = "User role updated successfully";
+            return RedirectToAction("Index");
+        }
+
     }
 }
