38e385d2c3037bb5377b1fa8ed3bd3040762bf41
ADMIN <tt2324564@gmail.com>
2023-04-25 07:51:55 +0700

    done

--- OganiShop/Areas/Admin/Controllers/HomeController.cs
+++ OganiShop/Areas/Admin/Controllers/HomeController.cs
@@ -21,6 +21,11 @@ namespace OganiShop.Areas.Admin.Controllers
         }
         public IActionResult Index()
         {
+            ViewBag.OrderNumber = _dbContext.OrderDetails.Where(x => x.IsDeleted == false).Count();
+            ViewBag.CategoryNumber = _dbContext.Categories.Where(x => x.IsDeleted == false).Count();
+            ViewBag.UserNumber = _dbContext.Users.Where(x => x.IsDeleted == false).Count();
+            ViewBag.ProductNumber = _dbContext.Products.Where(x => x.IsDeleted == false).Count();
+
             return View();
         }
         public IActionResult GetAccount()
@@ -38,6 +43,32 @@ namespace OganiShop.Areas.Admin.Controllers
                                                     Name = x.FirstName + " " + x.LastName,
                                                 }).First();
             return Json(user);
+        }
+
+        public IActionResult GetCategoryOrderQuantity()
+        {
+            var query = (from b1 in _dbContext.Products
+                         join b2 in _dbContext.OrderDetails on b1.Id equals b2.ProductId
+                         join b3 in _dbContext.Categories on b1.CategoryId equals b3.Id
+                         where b2.IsDeleted == false
+                         group new { b1, b2, b3 } by new { b3.Id, b3.Name } into b5
+                         select new
+                         {
+                             label = b5.Key.Name,
+                             value = b5.Sum(x => x.b2.Quantity)
+                         }).ToList();
+            return Json(query);
+        }
+
+        public IActionResult GetNumberOfOrders()
+        {
+            var query = _dbContext.OrderDetails
+                .Where(x => x.CreatedDate != null)
+                .GroupBy(x => x.CreatedDate.Value.Date)
+                .Select(g => new { Date = g.Key, OrderCount = g.Count() })
+                .ToList();
+
+            return Json(query.ToList());
         }
     }
 }
